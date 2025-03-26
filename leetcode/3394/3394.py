5f63a466896c4a9f84e01263d4643fc8c7a84c9a
stop0128 <stoooooping@gamil.com>
2025-03-26 15:15:24 +0900

    3394 fix

--- leetcode/3394/3394.py
+++ leetcode/3394/3394.py
@@ -1,49 +1,27 @@
 from typing import List
 
 class Solution:
-    def can_divide_into_three_segments(self, segments: List[List[int]]) -> bool:
-        segments.sort()
-        
-        points = sorted(set(start for start, end in segments) | set (end for start, end in segments))
-        
-        for i in range(1, len(points) - 1):
-            for j in range(i + 1, len(points)):
-                cut1, cut2 = points[i], points[j]
-                
-                group1, group2, group3 = False, False, False
-                
-                for start, end in segments:
-                    if end <= cut1:
-                        group1 = True
-                    elif start >= cut2:
-                        group3 = True
-                    elif start >= cut1 and end <= cut2:
-                        group2 = True
-                    else:
-                        break
-                else:
-                    if group1 and group2 and group3:
-                        return True
-        
-        return False
-                
     def checkValidCuts(self, n: int, rectangles: List[List[int]]) -> bool:
-        xs = []
-        ys = []
+        def can_divide_into_three_segments(segments: List[List[int]]) -> bool:
+            segments.sort()
         
-        for rectangle in rectangles:
-            xs.append([rectangle[0], rectangle[2]])
+            section = 0
+            max_end = segments[0][1]
         
-        if self.can_divide_into_three_segments(xs):
-            return True
-        
-        for rectangle in rectangles:
-            ys.append([rectangle[1], rectangle[3]])
+            for start, end in segments:
+                if start >= max_end:
+                    section += 1
+                max_end = max(max_end, end)
+                
+            if section >= 2:
+                return True
+            else:
+                return False
             
-        if self.can_divide_into_three_segments(ys):
-            return True
-        
-        return False
+        xs = [[rectangle[0], rectangle[2]] for rectangle in rectangles]
+        ys = [[rectangle[1], rectangle[3]] for rectangle in rectangles]
+                
+        return can_divide_into_three_segments(xs) or can_divide_into_three_segments(ys)
     
 
 if __name__ == "__main__":
